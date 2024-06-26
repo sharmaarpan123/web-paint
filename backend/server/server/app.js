4ee17375c0258ca4ad5d84a609af63c8cda9dc3c
sharmaarpan143 <sharmaarpan143@gmail.com>
2024-06-26 23:33:58 +0530

    change 5

--- backend/server/server/app.js
+++ backend/server/server/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import path from "path";
 import logger from "morgan";
-import userRouter from "../routes/user.js";
-import AuthRouter from "../routes/auth.js";
-import mongoInit from "../models/index.js";
-import tokenVerificationMiddleWare from "../utils/tokenVerificationMiddleWare.js";
+import userRouter from "../../routes/user.js";
+import AuthRouter from "../../routes/auth.js";
+import mongoInit from "../../models/index.js";
+import tokenVerificationMiddleWare from "../../utils/tokenVerificationMiddleWare.js";
 import { config } from "dotenv";
 import cors from "cors";
-import user from "../models/user/user.js";
+import user from "../../models/user/user.js";
 
 const init = async () => {
   config();
